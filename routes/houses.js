aa3e85a36782d6df6af26027d55bc58176e07012
Bree Jackson <bjacksonsisterwolf@gmail.com>
2017-08-20 20:14:04 -0400

    finding characters added and buttons made for each person not in a house.

--- routes/houses.js
+++ routes/houses.js
@@ -5,9 +5,13 @@ const Character = require("../models/charSchema")
 mongoose.promise = global.promise
 
 module.exports = app => {
-  app.get("/enroll", (req, res) => {
-    Character.find({age: <19})
-
-    res.render("enroll")
+  app.get("/enroll", (request, response) => {
+    Character.find({ house: "", yearBorn: { $gt: 1998 } })
+      .then(characters => {
+        response.render("enroll", { characters })
+      })
+      .catch(err => {
+        console.log(err)
+      })
   })
 }
